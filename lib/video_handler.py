2e7df778a94a80c79b9cb04e24d3c3efbf317a2e
Ubuntu <ubuntu@ip-172-31-36-96.us-west-2.compute.internal>
2019-11-02 03:16:16 +0000

    Auto-generated slate works!

--- lib/video_handler.py
+++ lib/video_handler.py
@@ -2,16 +2,17 @@ import subprocess
 import shutil
 import os
 import urllib
+from random import randint
 from pprint import pprint
 from frameioclient import FrameioClient
 from dotenv import load_dotenv
 from pathlib import Path  # python3 only
 
 def render_and_upload_slate(**kwargs):
-    print(f"Resource ID: {kwargs['resource_id']}")
-    print(f"Timecode Burnin: {kwargs['timecode_burnin']}")
-    print(f"Project: {kwargs['project']}")
-    print(f"Client: {kwargs['client']}")
+    # print(f"Resource ID: {kwargs['resource_id']}")
+    # print(f"Timecode Burnin: {kwargs['timecode_burnin']}")
+    # print(f"Project: {kwargs['project']}")
+    # print(f"Client: {kwargs['client']}")
 
     # Create temp directory
     if os.path.isdir(os.path.join(os.path.curdir, "temp")):
@@ -37,33 +38,46 @@ def render_and_upload_slate(**kwargs):
     og_asset_url = asset_info['original']
 
     # Download original frame.io video
+    print("Downloading video...")
     urllib.request.urlretrieve(og_asset_url, os.path.join(os.path.curdir, 'temp', asset_info['name']))
+    print("Video downloaded. Continuing...")
+
+    generate_slate(client=kwargs['client'], fps=asset_info['fps'], duration=asset_info['duration'], project=kwargs['project'])
 
     # Upload new video to Frame.io
     final_video_path = ""
     upload_to_frameio(final_video_path)
     
     # Clean-up temp folder
-    shutil.rmtree("temp")
+    # shutil.rmtree("temp")
 
     return True
 
-def generate_slate():
+def generate_slate(**kwargs):
+    print("Generating slate...")
     # Crate slate w/ FFMPEG
-    movie_name = "slate.mp4"
+    movie_path = f"new_slate_{randint(1,100)}.mp4"
 
-    """
-    ffmpeg -i input.mp4 -vf drawtext="fontfile=/path/to/font.ttf: \
-    text='Stack Overflow': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: \
-    boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2" -codec:a copy output.mp4
-    """
+    ffmpeg_string = """-i lib/Slate_v01_blank.mp4 -vf \
+    'drawtext=fontfile=lib/AvenirNext.ttc: \
+    text={}:fontcolor=white:fontsize=62:box=0: \
+    x=1118:y=351, \
+    drawtext=fontfile=lib/AvenirNext.ttc: \
+    text={}:fontcolor=white:fontsize=62:box=0: \
+    x=1118: y=551, \
+    drawtext=fontfile=lib/AvenirNext.ttc: \
+    text={}:fontcolor=white:fontsize=62:box=0: \
+    x=1118: y=742'\
+    -an {}
+    """.format(kwargs['client'].upper(), kwargs['project'].upper(), kwargs['duration'], movie_path)
 
-    with open("/tmp/output.log", "a") as output:
+    with open("output.log", "a") as output:
         subprocess.call(
-            """docker run -v $(pwd):$(pwd) -w $(pwd) jrottenberg/ffmpeg:3.2-scratch -stats -i temp/img%03d.jpg -c:v libx264 -vf scale=4096:-1 -vf fps=8 -pix_fmt yuv420p temp/{}""".format(movie_name),
+            """docker run -v $(pwd):$(pwd) -w $(pwd) jrottenberg/ffmpeg:3.2-scratch -stats {}""".format(ffmpeg_string),
             shell=True, stdout=output, stderr=output
         )
-    
+
+    print("Slate generation completed")
     return
 
 def upload_to_frameio(final_video_path):
